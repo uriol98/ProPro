06ee954e7ca45ba6d8a708e94848275baa38e2e1
JereXO <38912666+JereXO@users.noreply.github.com>
2018-05-16 11:44:18 +0200

    Update Joc.java

--- Joc.java
+++ Joc.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.arraylist;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,5 +25,11 @@ public class Joc{
         public int indexPers(){
             return i;
         }
+		public void llegirF(){
+			List<String> lines = Files.readAllLines(Paths.get("c:\\temp\\testfile.txt"));
+			for (String line : lines) {
+				
+			}
+		}
         
 }
