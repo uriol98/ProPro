5c5c216394fc7d2a9b26cdb4138683777931708c
uriol98 <38912662+uriol98@users.noreply.github.com>
2018-05-21 11:56:50 +0200

    Update Paret.java

--- Paret.java
+++ Paret.java
@@ -7,48 +7,84 @@ import java.util.ArrayList;
  */
 
 /**
- *
- * @author oriol
+    @class Paret
+    @brief 
+    @author Oriol Canet
  */
 public class Paret {
     private ArrayList<Cela> cel;
     private Porta por;
     
-    //falta constructor
+   /**
+     @brief
+     @pre 
+     @post La sala s'ha creat amb una llargada de llarg i una amplada de ampl
+     */
     public Paret(){
     
         ArrayList<Cela> p = new ArrayList();
         cel=p;
     }
     
+    
+    /**
+     @brief
+     @pre 
+     @post La sala s'ha creat amb una llargada de llarg i una amplada de ampl
+     */
     public void AfegirCela(Cela c){
         cel.add(c);
     }
     
+    
+    /**
+     @brief
+     @pre 
+     @post La sala s'ha creat amb una llargada de llarg i una amplada de ampl
+     */
     public void afegirPorta(Porta p){
         por=p;
     }
     
-
+    /**
+     @brief
+     @pre 
+     @post La sala s'ha creat amb una llargada de llarg i una amplada de ampl
+     */
     public int mida(){
         return cel.size();
     }
 
+    
+    /**
+     @brief
+     @pre 
+     @post La sala s'ha creat amb una llargada de llarg i una amplada de ampl
+     */
     public Cela agafarCela(int pos){
             
             return cel.get(pos);
         }
     
+    /**
+     @brief
+     @pre 
+     @post La sala s'ha creat amb una llargada de llarg i una amplada de ampl
+     */
     public void mostrar(){
         for(int i=0; i<cel.size();i++){
             System.out.println(cel.get(i).posicio().CoordenadaX()+" "+cel.get(i).posicio().CoordenadaY());
         }
     }
+    
+    /**
+     @brief
+     @pre 
+     @post La sala s'ha creat amb una llargada de llarg i una amplada de ampl
+     */
+    public Porta getPorta(){
+        return por;
+    }
+    
 }
 
-
-
-
-    
-
-    
