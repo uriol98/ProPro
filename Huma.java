07bfbe77f17dc0dde658ef02b8d4273b0ef61075
JereXO <38912666+JereXO@users.noreply.github.com>
2018-05-27 23:04:12 +0200

    Update Huma.java

--- Huma.java
+++ Huma.java
@@ -14,17 +14,23 @@ public class Huma extends Personatge{
         private int memSize;
         private ArrayList<Clau> inventari;
 	
-        private ArrayListz<Int> memoria;
+        private ArrayList<Int> memoria;
       
 	private int nObjActual;
-        private boolean teTresor;
+        
 	
+        private Tresor tresor;
 	public Huma(){
+        //Pre:
+        //Post:
+        
             super();
             teTresor = false;
         }
         
         public Huma(String idPersonatge, String tipus, Posicio pos,int invSize,int memSize){
+        //Pre:
+        //Post:
             super(String idPersonatge, String tipus, Posicio pos);
             this.invSize = invSize;
             this.memSize = memSize;
@@ -32,18 +38,46 @@ public class Huma extends Personatge{
         }
         
 	public boolean afegirObjecte(Clau clau){
-            return(add(inventari clau));
+        //Pre:
+        //Post:
+            if(inventari.contains(clau)){
+                return(inventari.add(clau));
 	}
+            
+        public boolean afegirTresor(){
+        //Pre:
+        //Post:
         
-        @Override
-        public void moviment(Posicio pos2){
-        //post:acosta l'huma a la pos2 una casella
+            if (inventari.size < invSize){
+                tresor =  this.cela().Tresors();
+                                
+            }
+            
         }
-	
-	
-	public void decisio(){
-	//pre:
-	//post: l'huma ha pres una decisio
+        
+        
+ 
+        public void decisio(){
+        //Pre:torn de huma no controlat per usuari
+        //Post:tria quina es la millor accio 
+            if(tresor == null){
+                
+            }
+            
+            else if(this.cela().teTresor()){
+                this.afegirTresor();
+            }
+            else if(this.cela().teClau()){
+                this.afegirObjecte(this.cela().clau)
+            }
+            else if(this.cela().sala().contePersonatge()){
+                //fugir
+            }
+            
+        }
+        private void sortir(){
+            
+        }
         
         
 	
